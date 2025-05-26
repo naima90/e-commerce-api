73dc30328b5135f4304d85d05cc1272c739baaa8
Naima <naimaosman203@gmail.com>
2025-05-26 15:45:12 +0100

    application set up with express and ts

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import * as dotevnv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+dotevnv.config();
+
+if (!process.env.PORT) {
+  console.error("No port value is specified.. ");
+}
+
+const PORT = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+// listen for a port
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
